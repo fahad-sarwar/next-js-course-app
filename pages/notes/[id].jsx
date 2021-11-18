8745ee0fc14d0b5204b2f494e1221a3313ea7a55
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2021-11-18 11:41:24 +0000

    Implemented navigation, styling

---
+++ pages/notes/[id].jsx
@@ -0,0 +1,21 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import React from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+export default () => {
+    const router = useRouter()
+    const { id } = router.query
+
+    return(
+        <div sx={{variant: 'containers.page'}}>
+            <h1>Note: {id} </h1>
+
+            <Link href="/notes">
+                <a>Notes</a>
+            </Link>
+        </div>
+    )
+}
